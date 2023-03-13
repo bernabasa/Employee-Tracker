2c3ffffb38d6b980658c75b57a019a0b5a6cb1de
bernabasa <75853223+bernabasa@users.noreply.github.com>
2023-03-13 10:33:37 -0400

    creating tables

---
+++ db/schema.sql
@@ -0,0 +1,36 @@
+-- DATABASE CREATION--
+DROP DATABASE IF EXISTS company;
+
+CREATE DATABASE company;
+USE company;
+
+-- table creation--
+
+DROP TABLE IF EXISTS department;
+DROP TABLE IF EXISTS role;
+DROP TABLE IF EXISTS employee;
+
+CREATE TABLE role (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30) NOT NULL,
+    salaty DECIMAL NOT NULL,
+    department_id INTEGER NULL
+    CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id) ON UPDATE CASCADE ON DELETE SET NULL);
+
+
+CREATE TABLE department (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30) NOT NULL
+
+);
+
+CREATE TABLE employee (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INTEGER NOT NULL,
+    manager_id INTEGER NOT NULL.
+    CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES role(id) ON UPDATE CASCADE ON DELETE SET NULL,
+    CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employee(id) ON UPDATE CASCADE ON DELETE SET NULL
+    
+);
